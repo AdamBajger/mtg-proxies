1ad53762d4921ffd5409d4007199137e74b432d0
Adam Bajger <a.bajger@24vs.io>
2024-04-27 21:27:28 +0200

    refactor: rewrite for flexibility and readability
    - remove pdf case from matplotlib function, since PDF output is handled by a separate method
    - add cache CLI argument for the following cache refactor
    - fix methods being shadowed by their local variables
    - replace Argparse with CLick
    - rewrite dimensions for better accessibility
    - decompose printing methods into classes for better extensibility

--- mtgproxies/print_cards.py
+++ mtgproxies/print_cards.py
@@ -1,27 +1,62 @@
 from __future__ import annotations
 
+import abc
 from pathlib import Path
+import logging
+from logging import getLogger
+from typing import Any, Literal, Generator, Iterable
 
+import PIL
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.backends.backend_pdf import PdfPages
 from matplotlib.patches import Rectangle
-from tqdm import tqdm
 
+from nptyping import NDArray, Float, Int, Shape
+import numpy as np
+
+from fpdf import FPDF
+from PIL.Image import Image
+
+from tqdm import tqdm
 from mtgproxies.plotting import SplitPages
+from mtgproxies.dimensions import get_pixels_from_size_and_ppsu, MTG_CARD_INCHES, get_ppsu_from_size_and_pixels, \
+    MTG_CARD_MM, UNITS_TO_MM, Units, PAPER_SIZE
 
 image_size = np.array([745, 1040])
 
+logger = getLogger(__name__)
+
 
 def _occupied_space(cardsize, pos, border_crop: int, closed: bool = False):
-    return cardsize * (pos * image_size - np.clip(2 * pos - 1 - closed, 0, None) * border_crop) / image_size
+
+    old_result = cardsize * (pos * image_size - np.clip(2 * pos - 1 - closed, 0, None) * border_crop) / image_size
+
+    new_result = occupied_space_new(cardsize, pos, border_crop=border_crop, gap_size=0)
+    logger.info(f"Old result: {old_result}, New result: {new_result},\nDiff: {old_result - new_result}")
+    return old_result
+
+
+def occupied_space_new(card_size: NDArray[2, Float], row_cols: NDArray[2, Int], border_crop: float, gap_size: float = 0) -> NDArray[2, Float]:
+    """Calculate the space occupied by cards on a sheet.
+
+    Args:
+        card_size: Size of a card (in inches).
+        row_cols: Number of rows and columns of cards.
+        border_crop: How much to crop from the border of each card (in inches)
+        gap_size: Size of the gap between cards (in inches)
+
+    Returns:
+        Size of the occupied space (in inches).
+    """
+    cropped_card_size = card_size - 2 * border_crop
+    total_gap_area = (row_cols - 1) * gap_size
+    return row_cols * cropped_card_size + total_gap_area
 
 
 def print_cards_matplotlib(
     images: list[str | Path],
     filepath: str | Path,
-    papersize=np.array([8.27, 11.69]),
-    cardsize=np.array([2.5, 3.5]),
+    papersize=PAPER_SIZE["A4"]['in'],
+    cardsize=MTG_CARD_INCHES,
     border_crop: int = 14,
     interpolation: str | None = "lanczos",
     dpi: int = 600,
@@ -35,54 +70,61 @@ def print_cards_matplotlib(
         papersize: Size of the paper in inches. Defaults to A4.
         cardsize: Size of a card in inches.
         border_crop: How many pixel to crop from the border of each card.
+        interpolation: Interpolation method for resizing images.
+        dpi: Dots per inch for the output file.
+        background_color: Background color of the paper.
     """
     # Cards per figure
-    N = np.floor(papersize / cardsize).astype(int)
-    if N[0] == 0 or N[1] == 0:
+    dims = np.floor(papersize / cardsize).astype(int)
+    rows, cols = dims
+    if rows == 0 or cols == 0:
         raise ValueError(f"Paper size too small: {papersize}")
-    offset = (papersize - _occupied_space(cardsize, N, border_crop, closed=True)) / 2
+    # calculate the offset to center the cards on the paper
+    offset = (papersize - _occupied_space(cardsize, dims, border_crop, closed=True)) / 2
+
 
     # Ensure directory exists
     filepath = Path(filepath)
     filepath.parent.mkdir(parents=True, exist_ok=True)
 
-    # Choose pdf of image saver
-    if filepath.suffix == ".pdf":
-        saver = PdfPages
-    else:
-        saver = SplitPages
-
-    with saver(filepath) as saver, tqdm(total=len(images), desc="Plotting cards") as pbar:
+    with SplitPages(filepath) as saver, tqdm(total=len(images), desc="Plotting cards") as pbar:
         while len(images) > 0:
-            fig = plt.figure(figsize=papersize)
-            ax = fig.add_axes([0, 0, 1, 1])  # ax covers the whole figure
-            #  Background
+            # create a new figure of paper size and an axes that covers the whole figure
+            fig = plt.figure(figsize=papersize)  # type: ignore
+            ax = fig.add_axes(rect=(0, 0, 1, 1))  # 0,0 offset fractions, 1,1 size fractions
+
+            #  Background color
             if background_color is not None:
                 plt.gca().add_patch(Rectangle((0, 0), 1, 1, color=background_color, zorder=-1000))
 
-            for y in range(N[1]):
-                for x in range(N[0]):
+            for y in range(cols):
+                for x in range(rows):
                     if len(images) > 0:
                         img = plt.imread(images.pop(0))
+                        img_dpi = get_ppsu_from_size_and_pixels(pixel_values=img.shape[:2], size=cardsize)
 
                         # Crop left and top if not on border of sheet
-                        left = border_crop if x > 0 else 0
-                        top = border_crop if y > 0 else 0
-                        img = img[top:, left:]
+                        # left = border_crop if x > 0 else 0
+                        # top = border_crop if y > 0 else 0
+                        border_crop_pixels = get_pixels_from_size_and_ppsu(ppsu=img_dpi, size=border_crop)
 
-                        # Compute extent
-                        lower = (offset + _occupied_space(cardsize, np.array([x, y]), border_crop)) / papersize
+                        img = img[border_crop_pixels:, border_crop_pixels:]
+
+                        # Compute extent (the box that will be filled by the image) as [left, right, bottom, top]
+                        occupied_space_before = _occupied_space(cardsize, np.array([x, y]), border_crop)
+                        lower = (offset + occupied_space_before) / papersize
                         upper = (
                             offset
-                            + _occupied_space(cardsize, np.array([x, y]), border_crop)
-                            + cardsize * (image_size - [left, top]) / image_size
+                            + occupied_space_before
+                            + cardsize * (image_size - border_crop_pixels) / image_size
                         ) / papersize
-                        extent = [lower[0], upper[0], 1 - upper[1], 1 - lower[1]]  # flip y-axis
+                        # flip y-axis
+                        extent = (float(lower[0]), float(upper[0]), 1 - float(upper[1]), 1 - float(lower[1]))
 
                         plt.imshow(
                             img,
                             extent=extent,
-                            aspect=papersize[1] / papersize[0],
+                            # aspect=papersize[1] / papersize[0],
                             interpolation=interpolation,
                         )
                         pbar.update(1)
@@ -100,8 +142,8 @@ def print_cards_matplotlib(
 def print_cards_fpdf(
     images: list[str | Path],
     filepath: str | Path,
-    papersize=np.array([210, 297]),
-    cardsize=np.array([2.5 * 25.4, 3.5 * 25.4]),
+    papersize=PAPER_SIZE["A4"]['mm'],
+    cardsize=MTG_CARD_MM,
     border_crop: int = 14,
     background_color: tuple[int, int, int] = None,
     cropmarks: bool = True,
@@ -115,31 +157,33 @@ def print_cards_fpdf(
         cardsize: Size of a card in inches.
         border_crop: How many pixel to crop from the border of each card.
     """
-    from fpdf import FPDF
+
 
     # Cards per sheet
-    N = np.floor(papersize / cardsize).astype(int)
-    if N[0] == 0 or N[1] == 0:
+    dims = np.floor(papersize / cardsize).astype(int)
+    rows, cols = dims
+    if rows == 0 or cols == 0:
         raise ValueError(f"Paper size too small: {papersize}")
-    cards_per_sheet = np.prod(N)
-    offset = (papersize - _occupied_space(cardsize, N, border_crop, closed=True)) / 2
+    cards_per_sheet = np.prod(dims)
+    offset = (papersize - _occupied_space(cardsize, dims, border_crop, closed=True)) / 2
 
     # Ensure directory exists
     filepath = Path(filepath)
     filepath.parent.mkdir(parents=True, exist_ok=True)
 
     # Initialize PDF
-    pdf = FPDF(orientation="P", unit="mm", format="A4")
+    pdf = FPDF(orientation="P", unit="mm", format=papersize)  # type: ignore
 
     for i, image in enumerate(tqdm(images, desc="Plotting cards")):
-        if i % cards_per_sheet == 0:  # Startign a new sheet
+        if i % cards_per_sheet == 0:  # Starting a new sheet
             pdf.add_page()
             if background_color is not None:
                 pdf.set_fill_color(*background_color)
-                pdf.rect(0, 0, papersize[0], papersize[1], "F")
+                pdf.rect(0, 0, float(papersize[0]), float(papersize[1]), "F")
 
-        x = (i % cards_per_sheet) % N[0]
-        y = (i % cards_per_sheet) // N[0]
+        # Compute card position on sheet
+        x = (i % cards_per_sheet) % rows
+        y = (i % cards_per_sheet) // rows
 
         # Crop left and top if not on border of sheet
         left = border_crop if x > 0 else 0
@@ -154,7 +198,7 @@ def print_cards_fpdf(
                 # Crop image
                 plt.imsave(cropped_image, plt.imread(image)[top:, left:])
 
-        # Compute extent
+        # Compute extent (the box that will be filled by the image) as [x, y, width, height]
         lower = offset + _occupied_space(cardsize, np.array([x, y]), border_crop)
         size = cardsize * (image_size - [left, top]) / image_size
 
@@ -166,12 +210,199 @@ def print_cards_fpdf(
             pdf.set_line_width(0.05)
             pdf.set_draw_color(255, 255, 255)
             a = cardsize * (image_size - 2 * border_crop) / image_size
-            b = papersize - N * a
-            for x in range(N[0] + 1):
-                for y in range(N[1] + 1):
+            b = papersize - dims * a
+            for x in range(rows + 1):
+                for y in range(cols + 1):
                     mark = b / 2 + a * [x, y]
                     pdf.line(mark[0] - 0.5, mark[1], mark[0] + 0.5, mark[1])
                     pdf.line(mark[0], mark[1] - 0.5, mark[0], mark[1] + 0.5)
 
     tqdm.write(f"Writing to {filepath}")
-    pdf.output(filepath)
+    pdf.output(str(filepath))
+
+
+Bbox = tuple[float, float, float, float]  # (x, y, width, height)
+Lcoords = tuple[float, float, float, float]  # (x0, y0, x1, y1)
+
+
+class CardAssembler(abc.ABC):
+    """Base class for assembling cards into sheets."""
+
+    def __init__(
+            self,
+            paper_size: NDArray[2, Float],
+            card_size: NDArray[2, Float],
+            border_crop: float = 0,
+            crop_marks_thickness: float = 0,
+            cut_lines_thickness: float = 0,
+            fill_corners: bool = False,
+            background_color: tuple[int, int, int] | None = None,
+            page_safe_margin: float = 0,
+            units: Units = "mm",
+    ):
+        """Initialize the CardAssembler.
+
+        Args:
+            paper_size: Size of the paper in the specified units.
+            card_size: Size of a card in the specified units.
+            border_crop: How many units to crop from the border of each card.
+            crop_marks_thickness: Thickness of crop marks in the specified units. Use 0 to disable crop marks.
+            cut_lines_thickness: Thickness of cut lines in the specified units. Use 0 to disable cut lines.
+            fill_corners: Whether to fill in the corners of the cards.
+            background_color: Background color of the paper.
+            page_safe_margin: How much to leave as a margin on the paper.
+            units: Units to use for the sizes.
+        """
+        # self.mm_coeff = UNITS_TO_MM[units]
+        self.units = units
+        self.paper_size = paper_size
+        self.paper_safe_margin = page_safe_margin
+        self.card_size = card_size
+        self.border_crop = border_crop
+        self.crop_marks_thickness = crop_marks_thickness
+        self.cut_lines_thickness = cut_lines_thickness
+        self.background_color = background_color
+        self.fill_corners_ = fill_corners
+
+        # precompute some values
+        self.card_bbox_size = self.card_size - (self.border_crop * 2)
+        self.safe_printable_area = self.paper_size - (self.paper_safe_margin * 2)
+        self.grid_dims = ((self.safe_printable_area + self.cut_lines_thickness) //
+                          (self.card_bbox_size + self.cut_lines_thickness)).astype(int)
+        self.rows, self.cols = self.grid_dims
+        self.grid_bbox_size = self.card_bbox_size * self.grid_dims + self.cut_lines_thickness * (self.grid_dims - 1)
+        self.offset = (self.safe_printable_area - self.grid_bbox_size) / 2
+
+    @abc.abstractmethod
+    def assemble(self, card_image_filepaths: list[str | Path], output_filepath: str | Path):
+        ...
+
+    def process_card_image(self, card_image_filepath: str | Path) -> Image:
+        """Process an image for assembly.
+
+        Loads the image, fills in corners, crops the borders, etc.
+
+        Args:
+            card_image_filepath: Image file to process.
+        """
+        img = np.asarray(PIL.Image.open(card_image_filepath))
+        # fill corners
+        if self.fill_corners_:
+            img = self.fill_corners(img)
+        # crop the cards
+        ppsu = get_ppsu_from_size_and_pixels(pixel_values=img.shape[:2], size=self.card_size)
+        crop_px = get_pixels_from_size_and_ppsu(ppsu=ppsu, size=self.border_crop)
+        img = img[crop_px:, crop_px:]
+        return img
+
+    def fill_corners(self, img: NDArray[Shape["2"], Float]) -> NDArray[Shape["2"], Float]:
+        """Fill the corners of the card with the closest pixels around the corners to match the border color."""
+        logger.warning("Filling corners not implemented, returning original image.")
+        return img
+
+    def get_page_generators(
+            self,
+            card_image_filepaths: list[str | Path],
+
+    ) -> Generator[Generator[tuple[Bbox, NDArray]]]:
+        """This method is a generator of generators of bounding boxes for each card on a page and the page indices.
+
+        The method can be iterated over to get the bbox iterators for each page and its index.
+
+        Yields:
+            tuple[Generator[Bbox], int]: Generator of bounding boxes and page index.
+        """
+        per_sheet = self.rows * self.cols
+        remaining = card_image_filepaths
+        while remaining:
+            if len(remaining) >= per_sheet:
+                for_sheet = remaining[:per_sheet]
+                remaining = remaining[per_sheet:]
+            else:
+                for_sheet = remaining
+                remaining = []
+
+            yield self.get_bbox_generator(for_sheet)
+
+    def get_bbox_generator(self, cards_on_page: list[Path]) -> Generator[tuple[Bbox, Image]]:
+        """This method is a generator of bounding boxes for each card on a page.
+
+        Args:
+            cards_on_page: Number of cards on the page.
+
+        Yields:
+            Bbox: Bounding box for each card.
+        """
+        for i, card_image_filepath in enumerate(cards_on_page):
+            card_pos = np.array([i % self.rows, i // self.rows])
+
+            cut_lines_offset = self.cut_lines_thickness * card_pos
+            preceding_cards_offset = self.card_bbox_size * card_pos
+            card_offset = cut_lines_offset + preceding_cards_offset + self.paper_safe_margin + self.offset
+            image = self.process_card_image(card_image_filepath)
+
+            yield (*card_offset, *self.card_bbox_size), image
+
+    def get_line_generator(self) -> Generator[Lcoords]:
+        """This method is a generator of bounding boxes for each cut line on a page.
+
+        Yields:
+            Bbox: Bounding box for each cut line.
+        """
+        # Horizontal lines
+        for i in range(self.rows):
+            y_left = self.paper_safe_margin + self.offset[1] + i * (self.card_bbox_size + self.cut_lines_thickness)
+            y_right = y_left + self.card_bbox_size
+            yield 0, y_left, self.paper_size[0], y_left
+            yield 0, y_right, self.paper_size[0], y_right
+
+        for i in range(self.cols):
+            x_top = self.paper_safe_margin + self.offset[0] + i * (self.card_bbox_size + self.cut_lines_thickness)
+            x_bottom = x_top + self.card_bbox_size
+            yield x_top, 0, x_top, self.paper_size[1]
+            yield x_bottom, 0, x_bottom, self.paper_size[1]
+
+
+class FPDF2CardAssembler(CardAssembler):
+    """Class for assembling cards into sheets using FPDF."""
+
+    def assemble(self, card_image_filepaths: list[str | Path], output_filepath: str | Path):
+        total_cards = len(card_image_filepaths)
+        pages = np.ceil(total_cards / (self.rows * self.cols)).astype(int)
+        tqdm_ = tqdm(total=total_cards, desc="Plotting cards")
+
+        logger.info(f"Will print {total_cards} cards in {pages} pages in a {self.rows}x{self.cols} grid.")
+
+        # Ensure directory exists
+        output_filepath = Path(output_filepath)
+        output_filepath.parent.mkdir(parents=True, exist_ok=True)
+
+        # Initialize PDF
+        pdf = FPDF(orientation="P", unit=self.units, format=self.paper_size)  # type: ignore
+
+        for page_idx, bbox_gen in enumerate(self.get_page_generators(card_image_filepaths)):
+            tqdm_.set_description(f"Plotting cards (page {page_idx + 1}/{pages})")
+            pdf.add_page()
+            if self.background_color is not None:
+                pdf.set_fill_color(*self.background_color)
+                pdf.rect(0, 0, float(self.paper_size[0]), float(self.paper_size[1]), "F")
+
+            if self.crop_marks_thickness > 0.0:
+                tqdm_.set_description(f"Plotting crop marks (page {page_idx + 1}/{pages})")
+                pdf.set_line_width(self.crop_marks_thickness)
+                for line_coordinates in self.get_line_generator():
+                    pdf.line(*line_coordinates)
+
+            tqdm_.set_description(f"Plotting cards (page {page_idx + 1}/{pages})")
+            for bbox, image in bbox_gen:
+                pdf.image(name=PIL.Image.fromarray(image), x=bbox[0], y=bbox[1], w=bbox[2], h=bbox[3])
+                tqdm_.update(1)
+
+        tqdm.write(f"Writing to {output_filepath}")
+        pdf.output(str(output_filepath))
+
+
+
+
+
+
--- print.py
+++ print.py
@@ -1,9 +1,20 @@
 import argparse
+from pathlib import Path
+from typing import IO, TextIO, Literal, Any
 
+import click
 import numpy as np
+from nptyping import NDArray, Shape, Float32
+from webcolors import name_to_rgb, IntegerRGB
 
 from mtgproxies import fetch_scans_scryfall, print_cards_fpdf, print_cards_matplotlib
 from mtgproxies.cli import parse_decklist_spec
+from mtgproxies.decklists import Decklist
+from mtgproxies.dimensions import (
+    PAPER_SIZE,
+    MTG_CARD_MM, Units, UNITS_TO_MM
+)
+from mtgproxies.print_cards import FPDF2CardAssembler
 
 
 def papersize(string: str) -> np.ndarray:
@@ -16,82 +27,153 @@ def papersize(string: str) -> np.ndarray:
     raise argparse.ArgumentTypeError()
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Prepare a decklist for printing.")
-    parser.add_argument(
-        "decklist",
-        metavar="decklist_spec",
-        help="path to a decklist in text/arena format, or manastack:{manastack_id}, or archidekt:{archidekt_id}",
-    )
-    parser.add_argument("outfile", help="output file. Supports pdf, png and jpg.")
-    parser.add_argument("--dpi", help="dpi of output file (default: %(default)d)", type=int, default=300)
-    parser.add_argument(
-        "--paper",
-        help="paper size in inches or preconfigured format (default: %(default)s)",
-        type=papersize,
-        default="a4",
-        metavar="WIDTHxHEIGHT",
-    )
-    parser.add_argument(
-        "--scale",
-        help="scaling factor for printed cards (default: %(default)s)",
-        type=float,
-        default=1.0,
-        metavar="FLOAT",
-    )
-    parser.add_argument(
-        "--border_crop",
-        help="how much to crop inner borders of printed cards (default: %(default)s)",
-        type=int,
-        default=14,
-        metavar="PIXELS",
-    )
-    parser.add_argument(
-        "--background",
-        help='background color, either by name or by hex code (e.g. black or "#ff0000", default: %(default)s)',
-        type=str,
-        default=None,
-        metavar="COLOR",
-    )
-    parser.add_argument("--cropmarks", action=argparse.BooleanOptionalAction, default=True, help="add crop marks")
-    parser.add_argument(
-        "--faces",
-        help="which faces to print (default: %(default)s)",
-        choices=["all", "front", "back"],
-        default="all",
-    )
-    args = parser.parse_args()
+def papersize_click_option(
+        ctx: click.Context,
+        param: click.Parameter,
+        value: str
+) -> str | NDArray[Shape["2"], Float32]:
+    spec = value.upper()
+    if spec in PAPER_SIZE:
+        return spec
+    elif "x" in spec:
+        split = spec.split("x")
+        if len(split) == 2:
+            return np.asarray([float(split[0]), float(split[1])] , dtype=float)
+        else:
+            raise click.BadParameter(
+                "Paper size must be in the format WIDTHxHEIGHT", param=param.name, ctx=ctx)
 
-    # Parse decklist
-    decklist = parse_decklist_spec(args.decklist)
+    else:
+        raise click.BadParameter(
+            f"Paper size not supported: {spec}. Try one of {PAPER_SIZE.keys()}"
+            f"or define the dimensions in a WIDTHxHEIGHT format", param=param.name, ctx=ctx)
+
+
+@click.group(name="print")
+@click.pass_context
+def ur_mom(ctx):
+    ctx.ensure_object(dict)
+
+
+def common_cli_arguments(func):
+    func = click.argument(
+        "output_file",
+        type=click.Path(path_type=Path, exists=False, writable=True),
+        required=True
+    )(func)
+    func = click.argument("deck_list", type=str, nargs=-1)(func)
+    func = click.option(
+        "--crop-mark-thickness", "-cm",
+        type=float, default=0.0,
+        help="Thickness of crop marks in the specified units. Use 0 to disable crop marks.",
+    )(func)
+    func = click.option(
+        "--cut-lines-thickness", "-cl",
+        type=float, default=0.0,
+        help="Thickness of cut lines in the specified units. Use 0 to disable cut lines."
+    )(func)
+    func = click.option(
+        "--crop-border", "-cb",
+        type=float, default=0.0,
+        help="How much to crop the borders of the cards in the specified units."
+    )(func)
+    func = click.option(
+        "--background-color", "-bg",
+        type=name_to_rgb, default=None,
+        help="Background color of the cards, either by name or by hex code."
+    )(func)
+    func = click.option(
+        "--paper-size", "-ps",
+        type=str, default="a4", callback=papersize_click_option,
+        help="Paper size keyword (A0 - A10) or dimensions in the format WIDTHxHEIGHT."
+    )(func)
+    func = click.option(
+        "--page-safe-margin", "-m",
+        type=float, default=0.0,
+        help="Margin around the area where no cards will be printed. Useful for printers that can't print to the edge."
+    )(func)
+    func = click.option(
+        "--faces", "-f",
+        type=click.Choice(["all", "front", "back"]), default="all",
+        help="Which faces to print."
+    )(func)
+    func = click.option(
+        "--units", "-u",
+        type=click.Choice(Units.__args__), default="mm",
+        help="Units of the specified dimensions. Default is mm."
+    )(func)
+    func = click.option(
+        "--fill-corners", "-fc",
+        is_flag=True,
+        help="Fill the corners of the cards with the colors of the closest pixels."
+    )(func)
+    func = click.option(
+        "--cache-dir", "-cd",
+        type=click.Path(path_type=Path, file_okay=False, dir_okay=True, writable=True),
+        default=Path.cwd() / ".cache" / "mtgproxies",
+        help="Directory to store cached card images."
+    )(func)
+    return func
+
+
+@ur_mom.command(name="pdf")
+@common_cli_arguments
+@click.pass_context
+def my_mom(
+        ctx: click.Context,
+        deck_list: list[str],
+        output_file: Path,
+        faces: Literal["all", "front", "back"],
+        crop_mark_thickness: float,
+        cut_lines_thickness: float,
+        crop_border: float,
+        background_color: IntegerRGB,
+        paper_size: str | NDArray[Shape["2"], Float32],
+        units: Units,
+        fill_corners: bool,
+        page_safe_margin: float,
+        cache_dir: Path
+):
+    """This command generates a PDF document at OUTPUT_FILE with the cards from the files in DECK_LIST
+
+    DECK_LIST is a list of files containing filepaths to decklist files in text/arena format
+    or entries in a manastack:{manastack_id} or archidekt:{archidekt_id} format.
+
+    OUTPUT_FILE is the path to the output PDF file.
+
+    """
+    parsed_deck_list = Decklist()
+    for deck in deck_list:
+        parsed_deck_list.extend(parse_decklist_spec(deck))
 
     # Fetch scans
-    images = fetch_scans_scryfall(decklist, faces=args.faces)
+    images = fetch_scans_scryfall(decklist=parsed_deck_list, faces=faces)
 
-    # Plot cards
-    if args.outfile.endswith(".pdf"):
-        import matplotlib.colors as colors
-
-        background_color = args.background
-        if background_color is not None:
-            background_color = (np.array(colors.to_rgb(background_color)) * 255).astype(int)
-
-        print_cards_fpdf(
-            images,
-            args.outfile,
-            papersize=args.paper * 25.4,
-            cardsize=np.array([2.5, 3.5]) * 25.4 * args.scale,
-            border_crop=args.border_crop,
-            background_color=background_color,
-            cropmarks=args.cropmarks,
-        )
+    # resolve paper size
+    if isinstance(paper_size, str):
+        if units in PAPER_SIZE[paper_size]:
+            resolved_paper_size = PAPER_SIZE[paper_size][units]
+        else:
+            resolved_paper_size = PAPER_SIZE[paper_size]["mm"] / UNITS_TO_MM[units]
     else:
-        print_cards_matplotlib(
-            images,
-            args.outfile,
-            papersize=args.paper,
-            cardsize=np.array([2.5, 3.5]) * args.scale,
-            dpi=args.dpi,
-            border_crop=args.border_crop,
-            background_color=args.background,
-        )
+        resolved_paper_size = paper_size
+    # Plot cards
+    printer = FPDF2CardAssembler(
+        units=units,
+        paper_size=resolved_paper_size,
+        card_size=MTG_CARD_MM,
+        crop_marks_thickness=crop_mark_thickness,
+        cut_lines_thickness=cut_lines_thickness,
+        border_crop=crop_border,
+        background_color=background_color,
+        fill_corners=fill_corners,
+        page_safe_margin=page_safe_margin
+    )
+
+    printer.assemble(card_image_filepaths=images, output_filepath=output_file)
+
+
+if __name__ == "__main__":
+    ur_mom(obj={})
+
+
